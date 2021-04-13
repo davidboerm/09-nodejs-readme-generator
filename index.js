014605e95895d5c202380d225aa3d0792f4b3604
davidboerm <davidboerm@gmail.com>
2021-04-13 17:26:14 -0500

    updated index.js & package.json

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
 // TODO: Create an array of questions for user input
 const questions = [];
