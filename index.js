adc993e300eac1d80283ddccadfb23b344025101
davidboerm <davidboerm@gmail.com>
2021-04-13 19:09:17 -0500

    updated index and generateMarkdown files

--- index.js
+++ index.js
@@ -1,28 +1,75 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const util = require('util');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+function promptUser() {
+  return inquirer.prompt([
+    {
+      name: 'title',
+      type: 'input',
+      message: 'Give your project a title.',
+    },
+    {
+      name: 'description',
+      type: 'input',
+      message: 'Briefly describe the project.',
+    },
+    {
+      name: 'installation',
+      type: 'input',
+      message: 'Add any installation instructions.',
+    },
+    {
+      name: 'usage',
+      type: 'input',
+      message: 'Add usage details.'
+    },
+    {
+      name: 'contributing',
+      type: 'input',
+      message: 'Add any directions for contribution.',
+    },
+    {
+      name: 'license',
+      type: 'list',
+      message: 'Choose license type.',
+      choices: ['Apache', 'BSD 3', 'BSD 2', 'GPL', 'MIT', 'none'],
+    },
+    {
+      name: 'test',
+      type: 'input',
+      message: 'Add any information regarding testing apps.',
+    },
+    {
+      name: 'email',
+      type: 'input',
+      message: 'Input your email address.',
+    },
+    {
+      name: 'linkedin',
+      type: 'input',
+      message: 'Input your LinkedIn profile link.',
+    },
+    {
+      name: 'github',
+      type: 'input',
+      message: 'Input your Github username(case sensitive).',
+    },
+  ]);
+}
+ 
+ // function to init app
+ function init() {
+  promptUser()
+    // .then((answers) => console.log(answers.license))
+    .then((answers) => writeFileAsync('./output/README.md', generateMarkdown(answers)))
+    .then(() => console.log('README.md successfully generated!'))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,81 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(answers) {
+  const licenseBadges = [{
+      name: 'Apache',
+      badge: '[![License: Apache](https://img.shields.io/badge/License-Apache-blue)](https://www.apache.org/licenses/LICENSE-2.0)'
+    },
+    {
+      name: 'BSD 3',
+      badge: '[![License: BSD3](https://img.shields.io/badge/License-BSD3-blue)](https://opensource.org/licenses/BSD-3-Clause)'
+    },
+    {
+      name: 'BSD 2',
+      badge: '[![License: BSD2](https://img.shields.io/badge/License-BSD2-blue)](https://opensource.org/licenses/BSD-2-Clause)'
+    },
+    {
+      name: 'GPL',
+      badge: '[![License: GPL](https://img.shields.io/badge/License-GPL-blue)](https://www.gnu.org/licenses/licenses.html)'
+    },
+    {
+      name: 'MIT',
+      badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-blue)](https://opensource.org/licenses/MIT)'
+    },
+    {
+      name: 'none',
+      badge: '',
+    }
+  ];
+  for (let i = 0; i < licenseBadges.length; i++) {
+    if (answers === licenseBadges[i].name) {
+      return licenseBadges[i].badge;
+    } 
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license section of README
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(answers) {
+  if (answers.license === 'none') {
+    return "";
+  } else {
+    return `
+## License
+${renderLicenseBadge(answers.license)}
+`;
+  }
+}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+// Function to generate markdown for README
+function generateMarkdown(answers) {
+  return `# ${answers.title}
+  
+${renderLicenseBadge(answers.license)}
+  
+## Description
+  ${answers.description}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#test)
+- [Questions](#questions)
+## Installation
+  ${answers.installation}
+## Usage
+  ${answers.usage}
+## Contributing
+  ${answers.contributing}
+${renderLicenseSection(answers)}
+## Tests
+  ${answers.test}
+## Questions
+  Any Questions? Please contact me at one of the following for more information:
+    
+  [Github](https://github.com/${answers.github})
+  [LinkedIn](${answers.linkedin})
+  [${answers.email}](mailto:${answers.email})
 `;
 }
 
